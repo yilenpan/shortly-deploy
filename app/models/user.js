d7e6e4c490583703dc6c7bc55f3f39310f876f3e
Yilen <yilen.pan@gmail.com>
2015-11-04 12:23:03 -0800

    Did cleanup

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,3 @@
-// var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
@@ -26,24 +25,3 @@ UserSchema.methods.comparePassword = function(attemptedPassword, cb) {
 }
 
 module.exports = mongoose.model('User', UserSchema)
-//
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
-// module.exports = User;
